353ec62da2febc2928511a95ba5ccb688cb4baa9
cactusJ12 <jang_sunwoong@student.smc.edu>
2023-02-26 23:01:58 -0800

    copy paste

---
+++ Project 1/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Project 1
+//
+//  Created by Sun Jang on 2023/02/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+
+}
+
